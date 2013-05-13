280812134bb9a09aa58dec6a2720b324cb51c0e9
Wout van Heeswijk <wout.van.heeswijk@gmail.com>
2013-05-13 17:47:17 +0200

    test md5 pw

--- manifests/server.pp
+++ manifests/server.pp
@@ -26,7 +26,7 @@ include puppios::params
         }
    # add a htpasswd user for nagios
 	htpasswd { "nagiosadmin":
- 	encryptpasswd => "$apr1$AAJvNhnI$nLg6q.oJBJE9nZQTd7pzP1",  # encrypted password hash goes here
+ 	cryptpasswd => "$apr1$AAJvNhnI$nLg6q.oJBJE9nZQTd7pzP1",  # encrypted password hash goes here
 	target => "${puppios::params::configdir}/htpasswd.users",
 }
 }
