8ec57c9768f86db91584ec5985a2cd196e58d912
Wout van Heeswijk <wout@ib.nl>
2014-03-12 16:13:00 +0100

    provide create_resource for puppet ENC

---
+++ manifests/resources/generic_unmanaged.pp
@@ -0,0 +1,9 @@
+#class to create resources based on puppios::target::generic_unmanaged for use with a puppet ENC (External Node Classifier).
+# it  is teste !only! with "the foreman"
+
+class puppios::resources::generic_unmanaged (
+  $hash_generic_unmanaged
+){
+
+	create_resource('puppios::target::generic_unmanaged', $hash_generic_unmanaged)
+}
