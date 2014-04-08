dedd58ec621ce4bafed0ab5feeef20778ab0cf92
Wout van Heeswijk <wout@ib.nl>
2014-04-08 17:08:45 +0200

    add test for auto check

--- manifests/checks/postgres/auto.pp
+++ manifests/checks/postgres/auto.pp
@@ -2,15 +2,20 @@ class puppios::checks::postgress::auto(
   $check_level = 'Class'
   )
 {
+  notify{"detecting postgres":}
   case $check_level{
     'Class': {
       #check for postgres class, if available add the check_postgres class
       if defined(Class['postgresql'])
       or defined(Class['ib::db'])    # Temporary hack for our specific needs!
         { 
+          notify{"detected postgres: Yeahhh":}
           if ! defined(Class['puppios::checks::postgres::checks']) {
           include puppios::checks::check_postgres
         }
+      }
+      else{
+        notify{"detected postgres: Noooow":}
       }
     }
   }
