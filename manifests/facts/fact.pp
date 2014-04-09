911d83ff86580ee1ff733f5bb19b3273dd2bca3d
Wout van Heeswijk <wout@ib.nl>
2014-04-09 17:45:16 +0200

    fixed single quotes to double quotes

--- manifests/facts/fact.pp
+++ manifests/facts/fact.pp
@@ -2,7 +2,7 @@ define puppios::facts::fact(
   $type = undef,
 ){
     @@concat::fragment{ "$type_$title_${::fqdn}":
-    target  => '/var/cache/puppios/facts.d/$type.list',
+    target  => "/var/cache/puppios/facts.d/$type.list",
     content => $title,
     tag     => $type,
   }
