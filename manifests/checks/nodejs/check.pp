55d28f5ae2aae2a39c57f28570ea02c135d0a076
Wout van Heeswijk <wout@ib.nl>
2014-04-09 17:31:03 +0200

    autocreate hostgroup

--- manifests/checks/nodejs/check.pp
+++ manifests/checks/nodejs/check.pp
@@ -1,10 +1,7 @@
 define puppios::checks::nodejs::check(
   ) {
   include puppios::params
-  @@concat::fragment{ "hostgroup_nodejs_${::fqdn}":
-    target  => '/var/cache/puppios/facts.d/hostgroups.list',
-    content => 'nodejs',
-    tag     => 'hostgroup'
-    #order   => '01'
+  puppios::resource::auto_hostgroup {'postgres_auto':}
+
   }
 }
