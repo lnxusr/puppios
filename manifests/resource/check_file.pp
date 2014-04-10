5fefefaa0a230474773e724ec6c892a520f1f806
Wout van Heeswijk <wout@ib.nl>
2014-04-10 12:21:46 +0200

    fixed typo of file source

--- manifests/resource/check_file.pp
+++ manifests/resource/check_file.pp
@@ -4,7 +4,7 @@ define puppios::resource::check_file (
   file {$name:
     path   => "$puppios::params::nagios_check_dir/$name",
     ensure => present,
-    source => 'puppet:////modules/puppios/checks/$nagios_check_name/${name}',
+    source => 'puppet:///modules/puppios/checks/$nagios_check_name/${name}',
     owner  => 'root',
     group  => 'root',
     mode   => '0755',
