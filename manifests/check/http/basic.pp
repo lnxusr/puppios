64786324fe1062855d3a83d3b20d5335a5a56897
Wout van Heeswijk <wout@ib.nl>
2014-04-11 12:00:31 +0200

    forgot to change variable name :-(

--- manifests/check/http/basic.pp
+++ manifests/check/http/basic.pp
@@ -1,7 +1,7 @@
 define puppios::check::http::basic(
   $http_basic_fqdn,
   $http_basic_host,
-  $http_basic_ssl,
+  $http_basic_ssl = false,
   $http_basic_url,
   $http_basic_string,
   $http_basic_timeout = '5',
@@ -10,35 +10,35 @@ define puppios::check::http::basic(
   {
   include puppios::params
 
-  if $http_ssl == false {
+  if $http_basic_ssl == false {
     $command_variable_ssl = undef
   }
-  elsif $http_ssl == true {
+  elsif $http_basic_ssl == true {
     $command_variable_ssl = '-S'
   }
   else {
-    fail("$http_ssl not a correct value, please use bolean values")
+    fail("$http_basic_ssl not a correct value, please use bolean values")
   }
 
   $http_expect_command = "--expect=\"${http_expect}\""
 
-  if $http_string == '' {
+  if $http_basic_string == '' {
     $http_string_command = ''
   }
   else {
-    $http_string_command = "-s \"$http_string\""
+    $http_string_command = "-s \"$http_basic_string\""
   }
 
-  $command_variable_string = "${http_string_command} ${http_expect_command}"
+  $command_variable_string = "${http_basic_string_command} ${http_expect_command}"
 
   nagios_command { "check_http_${title}":
     ensure       => 'present',
-    command_line => "/usr/lib/nagios/plugins/check_http $command_variable_ssl -H \'$http_fqdn\' -u $http_url $command_variable_string",
+    command_line => "/usr/lib/nagios/plugins/check_http $command_variable_ssl -H \'$http_basic_fqdn\' -u $http_url $command_variable_string",
     notify       => Service["${puppios::params::nagios_service}"]
   }
   nagios_service { "check_http_${title}":
     use                 => "generic-service",
-    service_description => "Check http ${title} ${http_fqdn} for ${http_string}",
+    service_description => "Check http ${title} ${http_basic_fqdn} for ${http_basic_string}",
     check_command       => "check_http_${title}!200.0,40%!400.0,80%",
     host_name           => "$http_host",
     notify              => Service["${puppios::params::nagios_service}"]
