fa6fb022a009a0a6386a9dd8fe49507c33feacdc
Wout van Heeswijk <wout.van.heeswijk@gmail.com>
2015-07-07 10:42:10 +0200

    - custom check_running_kernel based on ubuntu launchpad bug #1417133   (https://bugs.launchpad.net/ubuntu/+source/nagios-plugins-contrib/+bug/1417133)

--- manifests/check/linux/running_kernel.pp
+++ manifests/check/linux/running_kernel.pp
@@ -3,6 +3,14 @@ class puppios::check::linux::running_kernel {
     command => "check_running_kernel",
     runas   => 'root',
   }
+  file { '/usr/lib/nagios/plugins/check_running_kernel':
+    ensure => 'file',
+    source => 'puppet:///modules/puppios/checks/running_kernel/check_running_kernel',
+    mode   => '755',
+    owner  => 'root',
+    group  => 'root'
+  }
+  
   @@nagios_service { "check_running_kernel_${::fqdn}":
       check_command       => "check_nrpe_1arg!check_running_kernel",
       use                 => "generic-service",
