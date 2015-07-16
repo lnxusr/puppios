6f109d9d93afe8ec8a1b6da3dd2ab02596a5af29
Wout van Heeswijk <wout.van.heeswijk@gmail.com>
2015-07-16 11:37:17 +0200

    added "ensure" variable

--- manifests/target/generic_unmanaged.pp
+++ manifests/target/generic_unmanaged.pp
@@ -1,5 +1,6 @@
 define puppios::target::generic_unmanaged(
   $unmanaged_name                         = $title,
+  $unmanaged_ensure                       = 'present',
   $unmanaged_fqdn,
   $unmanaged_ipaddress,
   $unmanaged_operatingsystem,
@@ -9,7 +10,7 @@ define puppios::target::generic_unmanaged(
   include puppios::params
 
   nagios_host { $unmanaged_fqdn:
-    ensure               => present,
+    ensure               => $unmanaged_ensure,
     alias                => $unmanaged_name,
     address              => $unmanaged_ipaddress,
     use                  => "generic-host",
@@ -18,7 +19,7 @@ define puppios::target::generic_unmanaged(
   }
 
   nagios_hostextinfo { $unmanaged_fqdn:
-    ensure          => present,
+    ensure          => $unmanaged_ensure,
     icon_image_alt  => $unmanaged_operatingsystem,
     icon_image      => "base/$unmanaged_operatingsystem.png",
     statusmap_image => "base/$unmanaged_operatingsystem.gd2",
@@ -26,6 +27,7 @@ define puppios::target::generic_unmanaged(
   }
 
   nagios_service { "check_ping_${unmanaged_name}":
+    ensure               => $unmanaged_ensure,
     use                  => "generic-service",
     service_description  => "Check ping ${unmanaged_name}",
     check_command        => "check_ping!200.0,40%!400.0,80%",
